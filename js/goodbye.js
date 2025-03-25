862ef1d1119e819bf1b8113a3b4d1ddd132ab1db
Logan LaScola <loganmlascola@gmail.com>
2025-03-24 17:55:18 -0700

    commit

--- js/goodbye.js
+++ js/goodbye.js
@@ -4,27 +4,24 @@
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
 // See Lecture 52, part 2
 (function()  {
-
     // STEP 7: Create an object, called 'byeSpeaker' to which you will attach
     // the "speak" method and which you will expose to the global context
     // See Lecture 52, part 1
     // var byeSpeaker = 
-
     var byeSpeaker = {};
-
     // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
     var speakWord = "Good Bye";
-
     // STEP 8: Rewrite the 'speak' function such that it is attached to the
     // byeSpeaker object instead of being a standalone function.
     // See Lecture 52, part 2
     byeSpeaker.speak = function speak(name) {
         var outputDiv = document.getElementById("console-output"); // id from index.html
+
         var p = document.createElement("p"); // names in p tag
 
         p.textContent = speakWord + " " + name; // format of goodbye and names
 
-        outputDiv.appendChild(p); // list names for goodbye
+        outputDiv.appendChild(p); // gives positions for the names for goodbye to be printed out 
 
         console.log(speakWord + " " + name); // print names out
     };
