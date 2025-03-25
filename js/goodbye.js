738dad7e1cb32a9229ffe73af43c64252810aa6d
Logan LaScola <loganmlascola@gmail.com>
2025-03-24 17:45:20 -0700

    commit

--- js/goodbye.js
+++ js/goodbye.js
@@ -19,6 +19,18 @@
     // byeSpeaker object instead of being a standalone function.
     // See Lecture 52, part 2
     byeSpeaker.speak = function speak(name) {
+        var outputDiv = document.getElementById("console-output");
+        var p = document.createElement("p");
+
+        p.textContent = speakWord + " " + name;
+
+
+        // console.log(speakWord + " " + name);
+        outputDiv.appendChild(p);
+
+
+
+
         console.log(speakWord + " " + name);
     };
 
